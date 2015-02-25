98db9d3494d5595b448a87ecb67726f8d8fc3dec
Ricky Kendrick <Rek3650@yahoo.com>
2015-02-25 13:33:02 -0500

    Added an ArrayInfo object that calculates and stores some information about the provided array. Also added and AudioPlayer object that creates audio data from the provided ArrayInfo.

---
+++ javascript/arrayInfo.js
@@ -0,0 +1,36 @@
+//This object stores some basic information about an array
+function ArrayInfo(nArray)
+{
+  this.array = nArray;
+  this.sorted = this.isSorted();
+  this.trend = this.calcTrends();
+}
+
+//Figures out if the array is sorted
+ArrayInfo.prototype.isSorted = function()
+{
+  var trendUp = this.array[0] < this.array[this.array.length-1];
+  for(var i = 0; i < this.array.length-1; i++)
+  {
+    if(trendUp ? this.array[i] > this.array[i+1] : this.array[i] < this.array[i+1])
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+//Calculates some of the trends of the data
+ArrayInfo.prototype.calcTrends = function()
+{
+  var tmpTrend = {};
+  tmpTrend.min = Math.min.apply(Math, this.array);
+  tmpTrend.max = Math.max.apply(Math, this.array);
+  tmpTrend.sum = 0;
+  for(i = 0; i < this.array.length; i++)
+  {
+    tmpTrend.sum += this.array[i];
+  }
+  tmpTrend.avg = tmpTrend.sum/this.array.length;
+  return tmpTrend;
+}
