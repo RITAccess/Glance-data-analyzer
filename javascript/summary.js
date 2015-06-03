75effb912f9384ad13dacbe84eea72da4931b733
smcintosh881 <smcintosh881@gmail.com>
2015-06-03 16:08:25 -0400

    Minor visual format changes specifically spacings

--- javascript/summary.js
+++ javascript/summary.js
@@ -12,13 +12,13 @@ DataSummary.prototype.dataSummary = function() {
 
   // Writes the max, min, and average of each line in the graph
   for (var i = 0; i < this.currCollection.collection.length; i++) {
-      this.summaryDiv.innerHTML += " Line " + (i + 1) + " : Max: " + this.currCollection.collection[i].trend.max +
+      this.summaryDiv.innerHTML += " Line " + (i + 1) + ": Max: " + this.currCollection.collection[i].trend.max +
         " Min: " + this.currCollection.collection[i].trend.min +
         " Average: " + this.currCollection.collection[i].trend.avg + "</br></br>";
   }
   // Writes the max, min, and average of the total table data
-  this.summaryDiv.innerHTML += "Total Data Summary : Max: " + this.currCollection.max +
-    " Min: " + this.currCollection.min + " Average: " + this.calcCollectionAvg();
+  this.summaryDiv.innerHTML += "Total Data Summary: Max: " + this.currCollection.max +
+    " Min: " + this.currCollection.min + " Average: " + this.calcCollectionAvg() + "</br></br>";
 }
 
 // Calculates the average for the entire set of data
