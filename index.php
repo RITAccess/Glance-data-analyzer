1c372c29afdb20c4f952406fac1ed9723920d5f5
smcintosh881 <smcintosh881@gmail.com>
2015-06-18 15:20:56 -0400

    Had to add fieldset for accessibility rules

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="en">
 <head>
   <title>Data Analyzer</title>
   <meta charset="UTF-8">
@@ -16,13 +16,13 @@
   <header>
     <span id="title"><a href="index.php">Data Analyzer</a></span>
     <div class="inputBtnSection no-print">
-      <label class="fileUpload">
+      <label class="fileUpload" for="files">
           <input id="files" type="file" accept=".csv" class="upload" onfocus="focusElement('uploadBtn')" onblur = "blurElement('uploadBtn')"/>
           <span class="uploadBtn" onmouseover="focusElement('uploadBtn')" onmouseleave="blurElement('uploadBtn')">Load CSV</span>
       </label>
     </div>
     <div class="newBtnSection no-print">
-      <label class="createNew">
+      <label class="createNew" for="newTable">
         <input id="newTable" type="button" class="emptyTable" onfocus="focusElement('createBtn')" onblur="blurElement('createBtn')"/>
         <span class="createBtn" onmouseover="focusElement('createBtn')" onmouseleave="blurElement('createBtn')">Create New Table</span>
       </label>
@@ -53,12 +53,15 @@
   </div> <!-- end id="start" -->
   <div id="content" style="position: absolute; top: -9999px; left: -9999px;">
       <form id="rTypeSel" style="display:none;" class="no-print">
+        <fieldset id="radioOutline">
+          <legend>Change Graph Type</legend>
         <input id="lineRadioButton" type="radio" name="rGraphSel" value="Line" onclick="rType()">
         <label for="lineRadioButton">Line Graph</label>
         <input id="barRadioButton" type="radio" name="rGraphSel" value="Bar" onclick="rType()">
         <label for="barRadioButton">Bar Graph</label>
         <input id="scatterRadioButton" type="radio" name="rGraphSel" value="Scatter" onclick="rType()">
         <label for="scatterRadioButton">Scatter Plot</label>
+      </fieldset>
       </form>
       <div id="slider-range" data-start="0" data-end="-1" data-size="0" title="Chart Slider" class="no-print"></div>
       <svg id="overlay" width="800" height="400" style='display:none;' class="no-print">
@@ -81,7 +84,7 @@
         </div>
         <div id="columnLabel">
           <label><button id="subtractColumn" aria-label="Remove Column" onclick="subtractColumn()"> - </button></label>
-          <p style="display: inline">Column</p>
+          <p style="display: inline"> Column</p>
           <label><button id="addNewCol" aria-label="Add New Column" onclick="addColumn()"> + </button></label>
         </div>
         <button id="downloadCSV" onclick="download()">Download CSV</button>
